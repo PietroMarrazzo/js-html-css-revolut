bfedd4510f9fda006c705d67eccb6788555de122
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-28 15:29:22 +0100

    funzioni

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-    // click sul menu dropper
-    $('droppper-menu').click(function(){
+
+    // reference
+    var dropper = $('.droppper-menu > a');
+    var dropMenu = $('.droppper-menu .dropdown-menu');
+
+    // click sul menu dropper > show/hide menu
+    dropper.click(function(){
         dropmenu();
     })
     
 })
 
+
+// funzioni
